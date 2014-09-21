712f8a6a109ecb615f84d82a5dd5af7b51d7996a
梶原智之 <moguranosenshi@mogura-no-MacBookPro.local>
2014-09-21 23:06:04 +0900

    submission_data_v2.csvに対応

--- submission_p_read.py
+++ submission_p_read.py
@@ -12,18 +12,22 @@ def main():
     for line in fin:
         try:
             user_id, topic_id, p_read, p_share = line.strip().split("\t")
-            userid_topicid2probability[user_id][topic_id] = float(p_read)
+            if not topic_id:
+                topic_id = 0
+            userid_topicid2probability[int(user_id)][float(topic_id)] = float(p_read)
         except:
             pass
     fin.close()
     # submission_data
-    fin = open("submission_data.csv", "r")
+    fin = open("submission_data_v2.csv", "r")
     for line in fin:
         if "user_id" in line:
             print '"user_id","on_cid","p_topic_id","Answer"'
         else:
             user_id, on_cid, p_topic_id = line.strip().split(",")
-            if userid_topicid2probability[user_id][p_topic_id] > 0:
+            if p_topic_id == '""':
+                p_topic_id = 0
+            if userid_topicid2probability[int(user_id)][float(p_topic_id)] > 0:
                 print line.strip()+',"article_read"'
             else:
                 print line.strip()+',"NULL"'
